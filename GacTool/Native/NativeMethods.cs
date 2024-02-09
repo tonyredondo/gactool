5d3675afe5f77a3765af51f07d70cca3f1c55964
Tony Redondo <tony.redondo@datadoghq.com>
2024-02-09 13:49:10 +0100

    Add support for netcoreapp2.x (#4)

--- GacTool/Native/NativeMethods.cs
+++ GacTool/Native/NativeMethods.cs
@@ -5,8 +5,6 @@ using Microsoft.Win32;
 
 namespace GacTool.Native;
 
-#if NETCOREAPP3_0_OR_GREATER
-
 [ComVisible(false)]
 #if NET5_0_OR_GREATER
 [SupportedOSPlatform("windows")]
@@ -14,6 +12,8 @@ namespace GacTool.Native;
 internal sealed class NativeMethods
 {
     private const string NetFrameworkSubKey = @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\";
+
+    private delegate int CreateAssemblyCacheDelegate(out IAssemblyCache ppAsmCache, int reserved);
 
     internal static unsafe AssemblyCacheContainer CreateAssemblyCache()
     {
@@ -36,8 +36,8 @@ internal sealed class NativeMethods
 
         var libPointer = NativeLibrary.Load(fusionFullPath);
         var createAssemblyCachePointer = NativeLibrary.GetExport(libPointer, nameof(CreateAssemblyCache));
-        var functionPointer = (delegate* unmanaged[Stdcall]<out IAssemblyCache, uint, int>)createAssemblyCachePointer;
-        var hr = functionPointer(out var ppAsmCache, 0);
+        var createAssemblyCache = Marshal.GetDelegateForFunctionPointer<CreateAssemblyCacheDelegate>(createAssemblyCachePointer);
+        var hr = createAssemblyCache(out var ppAsmCache, 0);
         if (hr != 0)
         {
             NativeLibrary.Free(libPointer);
@@ -47,5 +47,3 @@ internal sealed class NativeMethods
         return new AssemblyCacheContainer(libPointer, ppAsmCache);
     }
 }
-
-#endif
